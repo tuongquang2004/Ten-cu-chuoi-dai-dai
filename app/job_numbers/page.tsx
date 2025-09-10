441cb3a8d2bc7fdc809a9c2fa15f9c055d6d90d5
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2025-09-10 14:57:06 +0700

    Add screen job_numbers so far

---
+++ app/job_numbers/page.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Breadcrumb from "@/components/Breadcrumb";
+import CommonButton from "@/components/CommonButton";
+import Layout from "@/components/Layout";
+import PageHeader from "@/components/PageHeader";
+import SearchBar from "@/components/SearchBar";
+import { Icon } from '@/components/Icon';
+
+const addItem = () => {
+    alert('You clicked a button :D');
+  }
+
+export default function JobNumbers() {
+    const breadcrumbs = [
+        { label: 'Settings', href: '/' },
+        { label: 'Manage Lists', href: '/' },
+        { label: 'Job Numbers', href: '/' }
+    ]
+    return (
+        <Layout>
+            <div className="px-6">
+                <div className="px-6">
+                    <Breadcrumb crumbs={[...breadcrumbs]}></Breadcrumb>
+                </div>
+                <PageHeader title="Manage Job Numbers" subtitle="Create or Edit Job Numbers" />
+                <div className="px-6 ml-auto flex items-center gap-3">
+                <SearchBar placeholder='Search Job Numbers' variant='third' icon_align='left' size = 'xl' className='min-w-[250px]'/>
+                <CommonButton variant='square' size = 'xl' onClick={addItem}><Icon.Search2 /></CommonButton>
+                <CommonButton variant="outline">Import</CommonButton>
+                </div>
+            </div>
+        </Layout>
+    )
+}
