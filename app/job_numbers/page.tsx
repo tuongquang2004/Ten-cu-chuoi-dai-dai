4aeb9c36341890b5b2ce01e121dfb1165d8e70be
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2025-09-11 16:15:06 +0700

    resolve/Breadcrumbs-pageJobNumbers

--- app/job_numbers/page.tsx
+++ app/job_numbers/page.tsx
@@ -5,7 +5,7 @@ import CommonButton from "@/components/CommonButton";
 import Layout from "@/components/Layout";
 import PageHeader from "@/components/PageHeader";
 import SearchBar from "@/components/SearchBar";
-import { Icon } from '@/components/Icon';
+import Search2 from '@/public/assets/icons/search2.svg';
 
 const addItem = () => {
     alert('You clicked a button :D');
@@ -17,12 +17,12 @@ export default function JobNumbers() {
             <div className="px-6">
                 <div className="px-6">
                     <Breadcrumb current="Job Numbers"/>
+                    <PageHeader title="Manage Job Numbers" size="xl" subtitle="Create or Edit Job Numbers" />
                 </div>
-                <PageHeader title="Manage Job Numbers" subtitle="Create or Edit Job Numbers" />
                 <div className="px-6 ml-auto flex items-center w-full">
-                    <div className="flex items-center gap-3 flex-1">
+                    <div className="flex items-center gap-3 py-6 flex-1">
                         <SearchBar placeholder='Search Job Numbers' variant='third' icon_align='left' size = 'xl' className='min-w-[250px]'/>
-                        <CommonButton variant='square' size = 'xl' onClick={addItem}><Icon.Search2 /></CommonButton>
+                        <CommonButton variant='square' size = 'xl' onClick={addItem}><Search2 /></CommonButton>
                     </div>
                     <div className="flex items-center gap-3">
                         <CommonButton variant="outline" size = 'button' >Import</CommonButton>
