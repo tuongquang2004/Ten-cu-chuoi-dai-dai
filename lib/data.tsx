91d5207dcd67fafbc254b03fd8078204fd5d00b9
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2025-09-11 15:23:57 +0700

    update data.tsx

--- lib/data.tsx
+++ lib/data.tsx
@@ -7,8 +7,8 @@ export type TileItemData = { label: string; icon: IconKey; href?: string }
 export const manageTiles: TileItemData[] = [
   { label: 'Referral Sources', icon: 'Referral', href: '/referral_sources' },
   { label: 'Shipping Methods', icon: 'Truck', href: '/' },
-  { label: 'Payment Methods', icon: 'Bag' },
-  { label: 'Job Numbers', icon: 'Briefcase', href: '/' },
+  { label: 'Payment Methods', icon: 'Bag', href: '/' },
+  { label: 'Job Numbers', icon: 'Briefcase', href: '/job_numbers' },
   { label: 'Exchange Rates', icon: 'Exchange', href: '/' },
   { label: 'Comments', icon: 'Note', href: '/' },
   { label: 'Special Pricing Schedule', icon: 'Tag', href: '/' },
