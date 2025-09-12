64cca464e7bacb05f83ee630ec3be78c91141cfa
Quang <tuong-quang.nguyen.etu@univ-lille.fr>
2025-09-12 19:35:23 +0700

    feature/be json datas for table

--- app/job_numbers/page.tsx
+++ app/job_numbers/page.tsx
@@ -13,6 +13,7 @@ import CommonTable, { createColumns } from "@/components/CommonTable";
 import { JobNumberRow } from "@/lib/data";
 // import RightBar from "@/components/RightBar";
 import { useState } from "react";
+import jobNumbers from '@/data/job_numbers.json';
 
 const addItem = () => {
     alert('You clicked a button :D');
@@ -22,38 +23,7 @@ export default function JobNumbers() {
     // const { source, setSource } = useSource();
     // const [isShow, setIsShow] = useState<boolean>(false);
 
-    const testData: JobNumberRow[]= [
-            { jobnumber: 'a', level: 1, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: true },
-            { jobnumber: 'b', level: 2, name: 'Test Job', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'c', level: 2, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: true },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-            { jobnumber: 'd', level: 3, name: 'Allen Test Job 1', startdate: '25 July 2025', enddate: '25 July 2025', isActive: false },
-        ]
+        const data: JobNumberRow[] = jobNumbers as JobNumberRow[];
     
         const testHeader = createColumns<JobNumberRow>()([
             { key: "jobnumber", label:"Job Number"},
@@ -86,10 +56,9 @@ export default function JobNumbers() {
 
         const [page, setPage] = useState(1);
         const [perPage, setPerPage] = useState(25);
-
-        const pageCount = Math.max(1, Math.ceil(testData.length / perPage));
+        const pageCount = Math.max(1, Math.ceil(data.length / perPage));
         const start = (page - 1) * perPage;
-        const pageRows = testData.slice(start, start + perPage);
+        const pageRows = data.slice(start, start + perPage);
 
     return (
         <Layout>
